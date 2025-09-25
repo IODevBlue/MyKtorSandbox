08ba4d37da7ca0f6875aa848d0016808f8d9ffff
Emmanuel E <iodevblue@gmail.com>
2025-09-25 01:44:12 +0100

    feat: Initialize Ktor project with Gradle build files
    Adds the initial set of Gradle build and configuration files for a Ktor project. This includes:
    - `gradlew` and `gradlew.bat` wrapper scripts.
    - `.gitignore` file with common patterns for Java/Kotlin projects.
    - `gradle.properties` to set Kotlin code style.
    - `build.gradle.kts` with Ktor, Kotlinx Serialization, and Kotest dependencies.
    - `settings.gradle.kts` to define the project name and configure repositories.
    - `README.md` providing an overview, features, and build/run instructions.

---
+++ build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.ktor)
+    alias(libs.plugins.plugin.serialization)
+}
+
+// group = "com.blueiobase.api.cloud.ktor"
+group = "io.github.iodevblue.sandbox.ktor.playground"
+version = "0.0.1"
+
+application {
+    mainClass = "io.ktor.server.netty.EngineMain"
+}
+
+dependencies {
+    implementation(libs.ktor.server.core)
+    implementation(libs.ktor.server.netty)
+    implementation(libs.logback.classic)
+    implementation(libs.ktor.server.core)
+    implementation(libs.ktor.server.config.yaml)
+    testImplementation(libs.ktor.server.test.host)
+    testImplementation(libs.kotlin.test.junit)
+    implementation(libs.kotest.property)
+    implementation(libs.kotest.assertions.core)
+    implementation(libs.kotest.runner.junit5)
+    implementation(libs.ktor.serialization.kotlinx.json)
+    implementation(libs.ktor.client.core)
+    implementation(libs.ktor.client.cio)
+    implementation(libs.ktor.client.content.negotiation)
+
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,7 @@
+rootProject.name = "MyKtorSandbox"
+
+dependencyResolutionManagement {
+    repositories {
+        mavenCentral()
+    }
+}
